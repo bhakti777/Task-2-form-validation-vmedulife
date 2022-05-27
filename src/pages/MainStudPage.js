35bd256ff18dfa9b30b24c8d83947aad4990574d
bhakti777 <bhakti1605@gmail.com>
2022-05-27 18:13:23 +0530

    created edit form

--- src/pages/MainStudPage.js
+++ src/pages/MainStudPage.js
@@ -13,6 +13,7 @@ class MainStudPage extends Component {
        super(props)
        this.state={
            showModal:false,
+           showModalEdit:false,
            formState: {
             userId:"",
             firstname: "",
@@ -25,8 +26,13 @@ class MainStudPage extends Component {
             zip: "",
           },
           users:{},
+          editUsers:{},
           isValid:false,
-          errorMsgEmail:""
+          errorMsgEmail:"",
+          errorMsgPassword:"",
+          selectedRow:[],
+          selectedStudentForEdit:{},
+          editMode:false
        }
    }
 
@@ -39,6 +45,18 @@ class MainStudPage extends Component {
    handleShowModal=()=>{
     this.setState({
         showModal:true
+    })
+   }
+
+   handleCloseModalEdit=()=>{
+    this.setState({
+        showModalEdit:false
+    })
+   }
+   handleShowModalEdit=(indexToEdit)=>{
+     let selectedStudent=this.state.selectedRow[indexToEdit]
+    this.setState({
+        showModalEdit:true
     })
    }
    
@@ -63,6 +81,25 @@ class MainStudPage extends Component {
                             })
                         }
                     }    
+
+                    if(event.target.name=="password"){
+
+                      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/g;
+                          const pswdpattern=passwordRegex.test(event.target.value)
+                          console.log("password pattern=>",pswdpattern)
+                          if(pswdpattern==true){
+                          this.setState({
+                              isValid:true,
+                              errorMsgPassword:""
+                          })
+                          }
+                          else{
+                              this.setState({
+                                  isValid:false,
+                                  errorMsgPassword:"Please enter a valid password!"
+                              })
+                          }
+                      }                        
 
         this.setState({
             formState:{...this.state.formState,[event.target.name]:event.target.value}
@@ -101,10 +138,75 @@ class MainStudPage extends Component {
         console.log("users",this.state.users)
       };
 
+      handleDelete=(indexToDelete)=>{
+      let usersClone=this.state.users
+      delete usersClone[indexToDelete]
+      this.setState({
+        users:usersClone
+      })
+      }
 
+    
+
+      handleOnChangeEdit=(event)=>{
+        console.log("input state edit",event.target.name,event.target.value);
+
+                if(event.target.name=="email"){
+
+                    const emailRegex = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+                        const pattern=emailRegex.test(event.target.value)
+                        console.log("pattern=>",pattern)
+                        if(pattern==true){
+                        this.setState({
+                            isValid:true,
+                            errorMsgEmail:""
+                        })
+                        }
+                        else{
+                            this.setState({
+                                isValid:false,
+                                errorMsgEmail:"Please enter a valid email address!"
+                            })
+                        }
+                    }    
+
+                    if(event.target.name=="password"){
+
+                      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/g;
+                          const pswdpattern=passwordRegex.test(event.target.value)
+                          console.log("password pattern=>",pswdpattern)
+                          if(pswdpattern==true){
+                          this.setState({
+                              isValid:true,
+                              errorMsgPassword:""
+                          })
+                          }
+                          else{
+                              this.setState({
+                                  isValid:false,
+                                  errorMsgPassword:"Please enter a valid password!"
+                              })
+                          }
+                      }                        
+
+        this.setState({
+            editUsers:{...this.state.editUsers,[event.target.name]:event.target.value}
+        })
+      }
+
+      handleOnCheckRow=(userId)=>{
+        console.log("user id=>", userId);
+        let selectedRowClone = this.state.selectedRow;
+     
+      console.log("selected student=>", this.state.selectedRow);
+      }
+
+      handleOnChangeAllSelect=()=>{
+
+      }
 
   render() {
-      const {showModal,formState,users,isValid,errorMsgEmail}=this.state
+      const {showModal,formState,users,isValid,errorMsgEmail,errorMsgPassword,showModalEdit,selectedRow}=this.state
 
     return(
         <>
@@ -116,7 +218,7 @@ class MainStudPage extends Component {
         </h5>
 
 
-       {/* MODAL : popup student registration form */}
+       {/* ADD MODAL : popup student registration form */}
 
          {showModal && 
         <Modal show={showModal} onHide={this.handleCloseModal} className="modal-width">
@@ -172,6 +274,7 @@ class MainStudPage extends Component {
                   name="password"
                   onChange={this.handleOnChange}
                 />
+                {!isValid && <span className="error">{errorMsgPassword}</span>}
               </Form.Group>
             </Row>
 
@@ -250,38 +353,200 @@ class MainStudPage extends Component {
       <Table striped bordered hover size="sm" className="table-width">
             <thead>
                 <tr>
-                <th>user-Id</th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Address</th>
-                <th>City</th>
-                <th>Gender</th>
-                <th>Zip</th>
+                    <th>
+                        <input type="checkbox"
+                        checked={selectedRow.length == Object.keys(users).length}
+                        onChange={this.handleOnChangeAllSelect}
+                        />
+                    </th>
+                    <th>user-Id</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>Gender</th>
+                    <th>Zip</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr>
-                    {Object.values(users).map((user)=>{
-                        return(
-                            <>
-                                <td>{user.userId}</td>
-                                <td>{user.firstname}</td>
-                                <td>{user.lastname}</td>
-                                <td>{user.email}</td>
-                                <td>{user.password}</td>
-                                <td>{user.address}</td>
-                                <td>{user.city}</td>
-                                <td>{user.gender}</td>
-                                <td>{user.zip}</td>
-                            </>
-                        )
-                    })}
-                </tr>
+                  {Object.values(users).map((user)=>{
+                      return(
+                          <tr>
+                              <td>
+                                  <input type="checkbox"
+                                  checked={selectedRow.includes(user.userId)}
+                                  onChange={() => this.handleOnCheckRow(user.userId)}
+                                  />
+                              </td>
+                              <td>{user.userId}</td>
+                              <td>{user.firstname}</td>
+                              <td>{user.lastname}</td>
+                              <td>{user.email}</td>
+                              <td>{user.password}</td>
+                              <td>{user.address}</td>
+                              <td>{user.city}</td>
+                              <td>{user.gender}</td>
+                              <td>{user.zip}</td>
+                              <td>
+                              <div className="flex">
+                                  <span>
+                                    <button type="button" className="btn btn-primary" onClick={()=>this.handleShowModalEdit(user.userId)}>
+                                      <span className="bi bi-pencil"></span>
+                                    </button>
+                                  </span>
+                                  <span className="btn-left">
+                                      <button type="button" className="btn btn-primary" onClick={()=>this.handleDelete(user.userId)}>
+                                        <span className='bi bi-x'></span>
+                                      </button>
+                                  </span>
+                              </div>
+                              </td>
+                          </tr>
+                      )
+                  })}
             </tbody>
       </Table>
+
+
+
+      {/* EDIT MODAL */}
+      {showModalEdit && 
+        <Modal show={showModalEdit} onHide={this.handleCloseModalEdit} className="modal-width">
+            <Modal.Header closeButton>
+              <h6>
+                <strong>Edit Student Registration</strong>
+              </h6>
+            </Modal.Header>
+            <Modal.Body>
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>userId:</Form.Label>
+              <Form.Control
+                placeholder="user Id"
+                disabled defaultValue={formState.userId}
+                name="userId"
+              />
+            </Form.Group>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridFirstName">
+                <Form.Label>First Name:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter firstname"
+                  value={formState.firstname}
+                  name="firstname"
+                  onChange={this.handleOnChangeEdit}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridLastName">
+                <Form.Label>Last Name:</Form.Label>
+                <Form.Control
+                  type="lastname"
+                  placeholder="Enter lastname"
+                  value={formState.lastname}
+                  name="lastname"
+                  onChange={this.handleOnChangeEdit}
+                />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={formState.email}
+                  name="email"
+                  onChange={this.handleOnChangeEdit}
+                />
+                {!isValid && <span className="error">{errorMsgEmail}</span>}
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={formState.password}
+                  name="password"
+                  onChange={this.handleOnChangeEdit}
+                />
+                {!isValid && <span className="error">{errorMsgPassword}</span>}
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Address:</Form.Label>
+              <Form.Control
+                placeholder="1234 Main St"
+                value={formState.address}
+                name="address"
+                onChange={this.handleOnChangeEdit}
+              />
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City:</Form.Label>
+                <Form.Control
+                  value={formState.city}
+                  name="city"
+                  onChange={this.handleOnChangeEdit}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Row>
+                  <Col xs={2}> Gender</Col>
+                  <Col>
+                    <Dropdown onSelect={this.handleOnSelect}>
+                      <Dropdown.Toggle
+                        variant="secondary"
+                        id="dropdown-basic"
+                        align="center"
+                      >
+                        Dropdown Button
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu  name="gender" value={formState.gender}>
+                        <Dropdown.Item href="#/action-1" name="female">Female</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2" name="male">Male</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3" name="other">Other</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </Col>
+                </Row>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control
+                  value={formState.zip}
+                  name="zip"
+                  onChange={this.handleOnChangeEdit}
+                />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" id="formGridCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+          <Button variant="primary" onClick={this.handleUpdate}>
+              Update
+            </Button>
+            <Button variant="secondary" onClick={this.handleCloseModalEdit}>
+              Close
+            </Button>
+          </Modal.Footer>
+          </Modal> 
+        }
+
+
       </div>
 
         </>
